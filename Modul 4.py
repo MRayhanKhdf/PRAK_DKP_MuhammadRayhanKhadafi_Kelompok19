e22edf0d4c61ee06b1a1e5e3a32083a416c6f2d3
Dagayashi <ebrilliantdewandaru@gmail.com>
2022-03-29 22:29:16 +0700

    edit commit

---
+++ Modul 4.py
@@ -0,0 +1,113 @@
+#Ebriliant
+#MAIN PROGRAM
+while True:
+    time.sleep(2)
+    print(" ")
+    print("LET'S PLAY A GAME, JUST ME AND YOU. ARE YOU READY?")
+    time.sleep(2)
+    print(" ")
+    user1 = input("PROCEED? [Y/N]\n")
+    user1 = user1.upper()
+    if user1 == "Y":
+        time.sleep(2)
+        print(" ")
+        print("THREE CORRECT ANSWERS AND THREE GUESSES FOR EACH QUESTIONS.")
+        time.sleep(2)
+        print(" ")
+        print("THEN, THE TRUTH SHALL BE SERVED IN YOUR HAND.")
+        time.sleep(2)
+        print(" ")
+        print("LET'S GET STARTED.")
+        user2 = None
+        trial1 = 1
+        ans_riddle_1 = "SECRETS"
+        while True:
+            time.sleep(2)
+            print(" ")
+            user2 = input("THE MORE I'M REVEALED, THE LESS I EXIST.\n")
+            user2 = user2.upper()
+            trial1 = trial1 + 1
+            if user2 == ans_riddle_1:
+                time.sleep(2)
+                print(" ")
+                print("YOU'RE SHOWING A HOPE IN THIS TRUTH-EXPOSAL. I'M ROOTING FOR YOU.")
+                user3 = None
+                trial2 = 1
+                ans_riddle_2 = "MASK"
+                while True:
+                    time.sleep(2)
+                    print(" ")
+                    user3 = input("FEAR HE WHO HIDES BEHIND ONE.\n")
+                    user3 = user3.upper()
+                    trial2 = trial2 + 1
+                    if user3 == ans_riddle_2:
+                        time.sleep(2)
+                        print(" ")
+                        print("THE TRUTH DOES NOT EVADE YOU. TRY ANOTHER AND SEE WHAT YOU UNCOVER.")
+                        user4 = None
+                        trial3 = 1
+                        ans_riddle_3 = "HONESTY"
+                        while True:
+                            time.sleep(2)
+                            print(" ")
+                            user4 = input("WHILE IT SHOULDN'T BE A CONTROVERSIAL POLICY, MANY OF GOTHAM'S ELITE PREFER TO DO THE OPPOSITE OF WHAT IT BEST.\n")
+                            user4 = user4.upper()
+                            trial3 = trial3 + 1
+                            if user4 == ans_riddle_3:
+                                time.sleep(2)
+                                print(" ")
+                                print("WHEN SHADOWS FALL, THE TRUTH RISES. YOU ARE CORRECT.")
+                                time.sleep(2)
+                                print(" ")
+                                print("I KNEW YOU'D MAKE IT.")
+                                time.sleep(2)
+                                print(" ")
+                                print("NOW, YOU'RE ABOUT TO ENTER THE TUNNEL.")
+                                time.sleep(2)
+                                print(" ")
+                                user5 = input("AS I PROMISE, WHAT TRUTH THAT YOU DESIRE AS YOUR LIGHT AT THE END OF THE TUNNEL?\n")
+                                user5 = user5.upper()
+                                if user5 == "THE RIDDLER":
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("WELL, MANY PEOPLE CALL ME THAT.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("MAYBE, BECAUSE I LOVE GIVING RIDDLES. OBVIOUSLY.")
+                                    user9 = input("")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print ("WHAT I WANT DOES NOT MATTER. WHAT GOTHAM DESERVES DOES MATTER.")
+                                    user10 = input("")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("JUSTICE.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("JUSTICE FOR EVERYTHING YOU'VE DONE TO ME!")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("JUSITCE FOR KEEPING YOUR BLIND EYE!")
+                                    time.sleep(2)
+                                    print(" ")
+                                    user11 = ("NOW TELL ME. WHAT DO YOU THINK YOU DESERVE?\n")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("OH I SEE, YOU'RE ONE OF THEM.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("YOU ARE THE PEOPLE WHO NEVER GO OUT OF THE CASTLE AND SEE THE REAL WORLD.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("WHILE YOU HOLDING A CHRISTMAS PARTY, I WAS BORN IN A MUDHOLE.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("NOW FEEL THE JUSTICE YOU DESERVE LIKE WHAT I'VE BEEN DOING TO YOUR ""AGENT"" OF JUSTICE.")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("https://www.youtube.com/watch?v=18RXhRfw95I&t=64s")
+                                    time.sleep(2)
+                                    print(" ")
+                                    print("GOOD BYE.")
+                                    break
+                                
